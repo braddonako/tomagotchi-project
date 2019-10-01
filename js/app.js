c5c016540759462f99e4f8413575ff77ef7052af
Brad Donakowski <braddonako@gmail.com>
2019-10-01 14:32:44 -0600

    made a class for the tomagotchi

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log('JavaScript locked and loaded baby');
+
+class tomagotchi {
+		constructor (Hunger, sleepiness, boredom, age){
+		this.hunger = hunger;
+		this.sleepiness = sleepiness;
+		this.boredom = boredom;
+		this.age = age;
+	}
+}
+
