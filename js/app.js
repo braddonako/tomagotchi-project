64538d712bd624b0aa6b18a921a8284b0c67fec5
Brad Donakowski <braddonako@gmail.com>
2019-10-02 11:04:42 -0600

    Created counter for age, hunger, sleep, and boredom

--- js/app.js
+++ js/app.js
@@ -1,75 +1,73 @@
 console.log('JavaScript locked and loaded baby');
 
-class tomagotchi {
+class Tomagotchi {
 		constructor (Hunger, sleepiness, boredom, age){
-		this.hunger = hunger;
-		this.sleepiness = sleepiness;
-		this.boredom = boredom;
-		this.age = age;
+		this.hunger = 1;
+		this.sleepiness = 1;
+		this.boredom = 1;
+		this.age = 1;
 	}
-}
-
-const Steven = new tomagotchi('0', '0', '0', '0');
-
-const gamePlay =  {
-	hunger: 0, // hungry goes up every 4
-	sleepiness: 0, // Every 7 seconds this will go up a point
-	boredom: 0,	// this will go up faster - goes up every one second
-	increaseHunger(){ // This is going to set an interval that will run up to 10
-		// once the hunger hits 10, the tomagotchi will die. -- I will have to create a function for the button that 
-		// will feed the alligator, and will reduce the point by one when clicked
-
+	increaseHunger(){
 		const interval = setInterval(() => {
-			$('#hunger').text(gamePlay.hunger++);
-			if (gamePlay.hunger === 10){
-			clearInterval(interval);	
+			$('#hungerCount').text(this.hunger++);
+			if (this.hunger === 11){
+			clearInterval(interval);
+			alert('Your tomigatchi has died from being too hungry. Please feed him next time');	
 			};
+		}, 4000) //run for each 7 seconds
 
-		}, 1000) //run for each second
-	},
-
-//---------------------------------------------------------------------------------------------
-// $(document).ready(function () {
-//     var number = parseInt($('#test').text(), 10) || 0; // Get the number from paragraph
-
-
-//     // Called the function in each second
-//     var interval = setInterval(function () {
-//         $('#test').text(number++); // Update the value in paragraph
-
-//         if (number > 1000) {
-//             clearInterval(interval); // If exceeded 100, clear interval
-//         }
-//     }, 1000); // Run for each second
-// });
-
-//---------------------------------------------------------------------------------------------
+	}
 	increaseSleepiness(){
 		//This method will run the same as check hunger. We will set the interbal to go up every 7 seconds
 		// If the interval hits 10, the tomigotchi will die
+	const interval = setInterval(() => {
+			$('#sleepinessCount').text(this.sleepiness++);
+			if (this.sleepiness === 11){
+			clearInterval(interval);
+			alert('Your tomigotchi has died from sleep deprivation. Next time, give him some nyquil');	
+			};
+		}, 7000) //run for each seven second
 
-
-	},
+	}
 	increaseBoredom() {
 		//This method will run the same as checkHunger and checkSleepiness, but will run at a different interval
-		//
-	},
+		const interval = setInterval(() => {
+			$('#boredomCount').text(this.boredom++);
+			if (this.boredom === 11){
+			clearInterval(interval);
+			alert('Your tomigotchi has died from being bored. You are a terrible owner.');	
+			};
+		}, 2000) //run for each seven second
+	}
 	increaseAge(){
-		// I will have a interval that will increase my pets age every 1 minute. 
-	},
+		// I will have a interval that will increase my pets age every 15 seconds.
+		const interval = setInterval(() => {
+			$('#ageCount').text(this.age++);
+			if (this.age === 11){
+			clearInterval(interval);
+			alert('Your tomigotchi has died from sleep deprivation. Next time, give him some nyquil');	
+			};
+		}, 15000) //run for each seven second 
+	}
 	feedMe(){
   	// I will create an event listener, that when the feedme button is clicked, 
   	// It will reduce the hunger by one. 
-  },
+  }
   turnOffLights(){
   	//The getSleep will have an event listener on the sleepiness button. 
   	// Each time this button is clicked, it will reduce the sleepiness by 1
-  },
+  }
   iAmBored(){
   	// I will create an event listener, that when the bored button is clicked, it will reduce the boredom by 1
   }
 }
 
+const steve = new Tomagotchi();
+
+// steve.increaseHunger();
+// steve.increaseSleepiness();
+// steve.increaseBoredom();
+// steve.increaseAge();
 
 
 
